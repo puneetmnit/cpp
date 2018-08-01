770173163a0f3749d2907b8479577c04ec1b36e3
puneetmnit <agrwalpuneet@gmail.com>
2018-08-01 18:06:01 +0530

    Create Utils.h
    Helper generic utility functions

---
+++ Utils.h
@@ -0,0 +1,17 @@
+/**@brief some generic helper functions and structs 
+*/
+
+namespace utils {
+
+//helper function to print type of give T
+template<class Stream, typename ...T>
+void print_type (Stream&& os, T&&... ) {
+    os << __PRETTY_FUNCTION__ << "\n";
+};
+
+template<class Stream, typename ...T>
+void print_type (Stream&& os) {
+    os << __PRETTY_FUNCTION__ << "\n";
+};
+
+} //namespace utils
