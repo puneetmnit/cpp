5b880b0032e1abad1297542dde9e5c33b5d26e8c
puneetmnit <agrwalpuneet@gmail.com>
2018-08-01 18:06:26 +0530

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -7,11 +7,11 @@ namespace utils {
 template<class Stream, typename ...T>
 void print_type (Stream&& os, T&&... ) {
     os << __PRETTY_FUNCTION__ << "\n";
-};
+}
 
 template<class Stream, typename ...T>
 void print_type (Stream&& os) {
     os << __PRETTY_FUNCTION__ << "\n";
-};
+}
 
 } //namespace utils
