d493b553d74e56f26aa5183ea01a5f151d1caa37
puneetmnit <agrwalpuneet@gmail.com>
2018-08-03 14:12:49 +0530

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -14,4 +14,20 @@ void print_type (Stream&& os) {
     os << __PRETTY_FUNCTION__ << "\n";
 }
 
+//print tuple
+namespace detail {
+    template<typename Stream, typename Tuple, size_t ...I>
+    void print_tup(Stream& os, Tuple&& tup, std::index_sequence<I...>) {
+        (void)std::initializer_list<int>{(os << std::get<I>(tup) << (I==sizeof...(I)-1?"": ","), 0)...} ;
+    }
+}
+
+template<typename Stream, typename ...Args>
+auto& operator<<(Stream& os, const std::tuple<Args...>& tup) {
+    os << "(";
+    print_tup(os, tup, std::make_index_sequence<sizeof...(Args)>());
+    os << ")";
+    return os;
+};
+
 } //namespace utils
